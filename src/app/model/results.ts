c58b31eefd04625e938c4e02d0b9c632ddb2d720
Maciej Wolak <maciejwolak@gmail.com>
2022-10-26 10:24:55 +0200

    2

--- src/app/model/results.ts
+++ src/app/model/results.ts
@@ -1,2 +1,20 @@
 export class Results {
+  id: number;
+  numberOfInfectedPeople: number;
+  numberOfHealthyPeople: number;
+  numberOfDiedPeople: number;
+  numberOfRecoveryPeople: number;
+  elapsedDay: number;
+  parameters: Parameters<any>;
+
+
+  constructor(id: number, numberOfInfectedPeople: number, numberOfHealthyPeople: number, numberOfDiedPeople: number, numberOfRecoveryPeople: number, elapsedDay: number, parameters: Parameters<any>) {
+    this.id = id;
+    this.numberOfInfectedPeople = numberOfInfectedPeople;
+    this.numberOfHealthyPeople = numberOfHealthyPeople;
+    this.numberOfDiedPeople = numberOfDiedPeople;
+    this.numberOfRecoveryPeople = numberOfRecoveryPeople;
+    this.elapsedDay = elapsedDay;
+    this.parameters = parameters;
+  }
 }
